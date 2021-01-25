aa1ff6ac943cb1658370dbbc0ba5b3a2e93a1991
NoahCardoza <noahcardoza@gmail.com>
2021-01-24 16:01:07 -0800

    build docs

--- en.search.min.6b7e134c2c7f00701ab122d750aa7d89a90514d9ffe9ada6c3e5ecdefd59352c.js
+++ en.search.min.6b7e134c2c7f00701ab122d750aa7d89a90514d9ffe9ada6c3e5ecdefd59352c.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/harvester-docs/flexsearch.min.js');loadScript('/harvester-docs/en.search-data.min.a0f15629d75a00023c841182e3364d8e8a926a223d162857e73b2f4f3deba96f.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/harvester-docs/flexsearch.min.js');loadScript('/harvester-docs/en.search-data.min.c60114939c77470cee8cea51cf9f82c2c93c6bf04d990479e3bd18aca5f31b70.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
